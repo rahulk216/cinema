1ca78002a078f433cec3938cb4196bd48c8d1538
Rahul Kakkadan <54850927+rahulk216@users.noreply.github.com>
2022-04-21 15:20:43 +0530

    footer imdb

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -4,18 +4,19 @@ import { FaFacebook } from 'react-icons/fa';
 import { FaInstagram } from 'react-icons/fa';
 import { FaWhatsapp } from 'react-icons/fa';
 import { FaVimeo } from 'react-icons/fa';
-
+import { FaImdb } from 'react-icons/fa';
 
 const Footer = () => {
 	return (
 		<footer className='footer-wrapper'>
 			<h1>contact: lalith@gmail.com</h1>
-			<hr />
+			<hr style={{ width: '80%', margin: 'auto' }} />
 			<div className='social-media'>
 				<FaFacebook />
 				<FaInstagram />
 				<FaWhatsapp />
-				<FaVimeo/>
+				<FaVimeo />
+				<FaImdb />
 			</div>
 		</footer>
 	);
