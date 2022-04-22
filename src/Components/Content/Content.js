14a49744caa269fd434334018959533d6a11a49c
shettyshubha21 <shubha.vs@robosoftin.com>
2022-04-22 16:01:16 +0530

    modal mweb and carousel

--- src/Components/Content/Content.js
+++ src/Components/Content/Content.js
@@ -1,12 +1,10 @@
 import React, { useRef } from "react";
 import "./Content.css";
+import HomepageSlider from "../HomepageSlider/HomepageSlider";
 import { FaPlayCircle } from "react-icons/fa";
+import { AiFillCloseCircle } from "react-icons/ai";
 import ReactPlayer from "react-player";
-
-//slide
-
-import { Splide, SplideSlide } from '@splidejs/react-splide';
-import '@splidejs/react-splide/css/skyblue';
+import { motion, AnimatePresence } from "framer-motion";
 
 const Content = ({ data }) => {
   const [showPlayer, setShowPlayer] = React.useState();
@@ -22,6 +20,7 @@ const Content = ({ data }) => {
 
   return (
     <div className="content-wrapper">
+      <HomepageSlider />
       {data &&
         data.map((item, index) => {
           return (
@@ -43,61 +42,97 @@ const Content = ({ data }) => {
                     />
                   </div>
                 </div>
-                {showModal && showPlayer === index + 1 && (
-                  <div className="overlay" onClick={closeModal} ref={modalRef}>
-				  <div className="overlay-wrapper">
-                   {item.video? <ReactPlayer
-                      url={item.video}
-                      playing={true}
-                      width="88.2vw"
-                      height="30vh"
-                      config={{
-                        youtube: {
-                          playerVars: {
-                            autoplay: 1,
-                            controls: 1,
+                <AnimatePresence>
+                  {showModal && showPlayer === index + 1 && (
+                    <>
+                      <motion.div
+                        initial={{
+                          opacity: 0,
+                        }}
+                        animate={{
+                          opacity: 1,
+                          transition: {
+                            duration: 0.3,
                           },
-                        },
-                      }}
-                    /> : <img src="https://images.unsplash.com/photo-1650374471470-a46867a3ce06?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyM3x8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60" alt="fallback" className="fallbackImage"></img> } 
-                    {item.title && <h1 className="title">{item.title}</h1>}
-                    {item.time && <p>{item.time}</p>}
-					<div className="details">
-					{item.director && <h1>{`Director: ${item.director}`}</h1>}
-					{item.DOP && <h1>{`DOP: ${item.DOP}`}</h1>}
-					{item.productions && <h1>{`Production: ${item.productions}`}</h1>}
-					</div>
-                  </div>
-				  </div>
-                )}
+                        }}
+                        exit={{
+                          opacity: 0,
+                          transition: {
+                            delay: 0.3,
+                          },
+                        }}
+                        onClick={closeModal}
+                        ref={modalRef}
+                        className="overlay"
+                      >
+                        <div
+                          className="close-btn"
+                          onClick={() => setShowModal(false)}
+                        >
+                          <AiFillCloseCircle color="black" fontSize="35px" />
+                        </div>
+                        <motion.div
+                          initial={{
+                            scale: 0,
+                          }}
+                          animate={{
+                            scale: 1,
+                            transition: {
+                              duration: 0.3,
+                            },
+                          }}
+                          exit={{
+                            scale: 0,
+                            transition: {
+                              delay: 0.3,
+                            },
+                          }}
+                          className="overlay-wrapper"
+                        >
+                          {item.video ? (
+                            <ReactPlayer
+                              url={item.video}
+                              playing={true}
+                              width="88.2vw"
+                              height="30vh"
+                              config={{
+                                youtube: {
+                                  playerVars: {
+                                    autoplay: 1,
+                                    controls: 1,
+                                  },
+                                },
+                              }}
+                            />
+                          ) : (
+                            <img
+                              src="https://images.unsplash.com/photo-1650374471470-a46867a3ce06?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyM3x8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60"
+                              alt="fallback"
+                              className="fallbackImage"
+                            ></img>
+                          )}
+                          {item.title && (
+                            <h1 className="title">{item.title}</h1>
+                          )}
+                          {item.time && <p className="time">{item.time}</p>}
+                          <div className="details">
+                            {item.director && (
+                              <h1>{`Director: ${item.director}`}</h1>
+                            )}
+                            {item.DOP && <h1>{`DOP: ${item.DOP}`}</h1>}
+                            {item.productions && (
+                              <h1>{`Production: ${item.productions}`}</h1>
+                            )}
+                          </div>
+                        </motion.div>
+                      </motion.div>
+                    </>
+                  )}
+                </AnimatePresence>
               </div>
             </div>
           );
         })}
-
-		<div className='pc'>
-				<Splide>
-					{data &&
-						data.map((item, index) => (
-							<div>
-								<SplideSlide>
-									<img
-										src={item.thumbnail.asset.url}
-										alt='thumbnail'
-                    className='slider-img'
-									/>
-									<div className='play-icon'>
-										<FaPlayCircle
-											fontSize='40px'
-											color='black'
-											className='play-icon'
-										/>
-									</div>
-								</SplideSlide>
-							</div>
-						))}
-				</Splide>
-			</div>
     </div>
   );
 };
