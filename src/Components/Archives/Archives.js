cfab89bad3c968a5e410e663048db8a17de458b0
shettyshubha21 <shubha.vs@robosoftin.com>
2022-04-28 15:10:58 +0530

    added stills

--- src/Components/Archives/Archives.js
+++ src/Components/Archives/Archives.js
@@ -1,60 +1,11 @@
-import React from 'react';
-import './archives.css';
-import { useQuery } from 'react-query';
-//sanity
-import sanityClient from '../../client.js';
+import React from "react";
+import Content from "../Content/Content";
+import { display } from "../Header/Header";
 
-import Lottie from 'react-lottie';
-import * as animationData from '../../images/lf30_editor_u6zvcjey.json';
+const Archives = ({ data, loader }) => {
+  const displayData = display("archives", data);
 
-const getStills = async () => {
-	return await sanityClient.fetch(`*[_type == "stills"]{
-		title,
-		thumbnail{
-			asset->{
-				_id,
-				url
-			},
-			alt
-		},
-	}`);
-};
-const defaultOptions = {
-	loop: true,
-	autoplay: true,
-	animationData: animationData,
-	rendererSettings: {
-		preserveAspectRatio: 'xMidYMid slice',
-	},
-};
-
-const Archives = () => {
-	const {  data, isLoading } = useQuery('stills', getStills);
-	console.log(isLoading);
-	console.log(data);
-	return (
-		<div className='container'>
-			{isLoading ? (
-				<div className='animation'>
-					<Lottie options={defaultOptions} height={100} width={100} />
-				</div>
-			) : (
-				<div>
-					<div className='stills-grid-container'>
-						{data?.map((item, index) => (
-							<div key={index} className='stills-grid-item'>
-								<div className='hover-overlay'>
-									<h2>{item.title}</h2>
-								</div>
-								<img src={item.thumbnail.asset.url} alt={item.title} />
-							</div>
-						))}
-					</div>
-					<div style={{ marginBottom: '8rem' }} />
-				</div>
-			)}
-		</div>
-	);
+  return <Content data={displayData} loader={loader} />;
 };
 
 export default Archives;
